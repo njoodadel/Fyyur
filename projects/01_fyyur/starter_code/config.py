d4cc0ed0db6740b5e18efb270415b2a87a7aa0e9
Njood <njoodalshaalan.66@gmail.com>
2020-12-22 21:59:34 +0300

    done artist & venue

--- projects/01_fyyur/starter_code/config.py
+++ projects/01_fyyur/starter_code/config.py
@@ -8,6 +8,5 @@ DEBUG = True
 
 # Connect to the database
 
-
 # TODO IMPLEMENT DATABASE URL
-SQLALCHEMY_DATABASE_URI = '<Put your local database url>'
+SQLALCHEMY_DATABASE_URI = 'postgresql://njoodadel:2514@localhost:5432/fyyur'
